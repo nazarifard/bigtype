3f41c96ab19dbdab9b0a16ec3d6091feccae346f
Bahador Nazarifard <bahador.nazarifard@gmail.com>
2025-02-02 19:27:00 +0330

    bug fix

--- internal/hash/hash.go
+++ internal/hash/hash.go
@@ -4,7 +4,7 @@ import (
 	"unsafe"
 
 	"github.com/nazarifard/bigtype/internal/utils"
-	marshal "github.com/nazarifard/marshaltap"
+	tap "github.com/nazarifard/marshaltap"
 )
 
 const defaultSeed uint64 = 0x7d5b016bcbfebb4c
@@ -50,7 +50,7 @@ func (h *Hash[K]) Hash(k K) uint64 {
 	return h.hashFn(k, h.seed)
 }
 
-type HashM[K any, M marshal.Interface[K]] struct {
+type HashM[K any, M tap.Tap[K]] struct {
 	seed      uint64
 	Marshaler M
 }
@@ -62,7 +62,7 @@ func (h *HashM[K, M]) Hash(k K) uint64 {
 	return n64
 }
 
-func NewHash[K any](m ...marshal.Interface[K]) Hashable[K] {
+func NewHash[K any](m ...tap.Tap[K]) Hashable[K] {
 	var k K
 	if utils.IsBytes(k) {
 		h := &BytesHash{
@@ -81,15 +81,15 @@ func NewHash[K any](m ...marshal.Interface[K]) Hashable[K] {
 		return any(h).(Hashable[K])
 
 	} else if len(m) == 1 {
-		h := &HashM[K, marshal.Interface[K]]{
+		h := &HashM[K, tap.Tap[K]]{
 			seed: defaultSeed,
 		}
 		return any(h).(Hashable[K])
 	} else {
-		var tap marshal.Interface[K] = nil //gob.GobTap[K]{}
-		h := &HashM[K, marshal.Interface[K]]{
+		var t tap.Tap[K] = nil //gob.GobTap[K]{}
+		h := &HashM[K, tap.Tap[K]]{
 			seed:      defaultSeed,
-			Marshaler: tap,
+			Marshaler: t,
 		}
 		return any(h).(Hashable[K])
 	}
